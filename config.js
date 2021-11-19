b3b07de113ade4c74a98fbbfa1b203f66eeb102b
Thunder X <72381397+Yash200717@users.noreply.github.com>
2021-11-19 07:16:48 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyA9SgN_gcQ-CnE4-5QLAOFxJ2_6GjdmNMI",
+    authDomain: "storytellingapp-f375c.firebaseapp.com",
+    projectId: "storytellingapp-f375c",
+    storageBucket: "storytellingapp-f375c.appspot.com",
+    messagingSenderId: "852751099414",
+    appId: "1:852751099414:web:cc6b1207406969b5e6b737",
+    measurementId: "G-EMR4DBZGVJ"
+  };
